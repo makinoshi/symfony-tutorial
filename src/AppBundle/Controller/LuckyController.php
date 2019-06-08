686248990eab77230a4262793e9b724d90e4f142
makinoshi <masa.kinoshita02@gmail.com>
2019-06-08 15:32:32 +0900

    Create your First Page in Symfony

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
+{
+
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number
+        ]);
+
+//        return new Response(
+//            '<html><body>Lucky number: '.$number.'</body></html>'
+//        );
+    }
+}
