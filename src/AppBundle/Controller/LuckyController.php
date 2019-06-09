41f3f3fd37c964526b24e7a4409aadce54e607ff
makinoshi <masa.kinoshita02@gmail.com>
2019-06-09 14:22:13 +0900

    Controller

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use Exception;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -10,11 +13,17 @@ class LuckyController extends Controller
 {
 
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number/{max}", requirements={"max"="\d+"})
+     * @param int $max
+     * @param LoggerInterface $logger
+     * @return Response
+     * @throws Exception
      */
-    public function numberAction()
+    public function numberAction($max = 100, LoggerInterface $logger)
     {
-        $number = random_int(0, 100);
+        $logger->info('We are logging!');
+
+        $number = random_int(0, $max);
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number
@@ -23,5 +32,16 @@ class LuckyController extends Controller
 //        return new Response(
 //            '<html><body>Lucky number: '.$number.'</body></html>'
 //        );
+    }
+
+    /**
+     * @Route("/lucky/number/json/{max}", requirements={"max"="\d+"})
+     * @param int $max
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function numberJsonAction($max = 100)
+    {
+        return $this->json(['number' => random_int(0, $max)]);
     }
 }
